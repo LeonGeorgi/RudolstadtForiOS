9a9b0044b2bc4f917f6105f5e6ac06693b472a28
Leon Georgi <leon@ifsr.de>
2020-03-23 13:41:38 +0100

    Add less smart recommendations

--- RudolstadtForiOS/Views/GeneratedScheduleView.swift
+++ RudolstadtForiOS/Views/GeneratedScheduleView.swift
@@ -11,15 +11,49 @@ import SwiftUI
 struct GeneratedScheduleView: View {
 
     @EnvironmentObject var dataStore: DataStore
+    @EnvironmentObject var settings: UserSettings
 
     @State private var showingSheet = false
     @State var selectedDay: Int = -1
+    @State var smartRecommendations = true
     @Binding var generatedEvents: [Event]
 
     var eventDays: [Int] {
         Set(dataStore.events.lazy.map { (event: Event) in
             event.festivalDay
         }).sorted(by: <)
+    }
+
+    func eventsToShow() -> [Event] {
+        if smartRecommendations {
+            return generatedEvents
+        } else {
+            let savedEvents = dataStore.events.filter { event in
+                settings.savedEvents.contains(event.id)
+            }
+            return dataStore.events.filter { event in
+                savedEvents.contains {
+                    $0.id == event.id
+                } || (
+                        settings.ratings.keys.contains(String(event.artist.id)) &&
+                                savedEvents.contains {
+                                    $0.artist.id != event.artist.id
+                                } && !intersects(events: savedEvents, current: event)
+                )
+            }
+        }
+    }
+
+    func intersects(events: [Event], current: Event) -> Bool {
+        return events.contains { event in
+            let collides = intersect(first: event, second: current)
+            return collides
+        }
+    }
+
+    func intersect(first: Event, second: Event) -> Bool {
+        first.festivalDay == second.festivalDay &&
+                !(first.startTimeInMinutes > second.endTimeInMinutes || first.endTimeInMinutes < second.startTimeInMinutes)
     }
 
     var body: some View {
@@ -33,12 +67,23 @@ struct GeneratedScheduleView: View {
                         .padding(.trailing, 10)
                         .pickerStyle(SegmentedPickerStyle())
 
-                List(generatedEvents.filter { (event: Event) in
+                List(eventsToShow().filter { (event: Event) in
                     event.festivalDay == selectedDay
                 }) { event in
-                    TimeProgramEventCell(event: event)
+                    Button(action: {
+                        if !self.settings.savedEvents.contains(event.id) {
+                            self.settings.savedEvents.append(event.id)
+                        }
+                    }) {
+                        TimeProgramEventCell(event: event)
+                    }.disabled(self.settings.savedEvents.contains(event.id))
                 }
             }.navigationBarTitle("Generated events", displayMode: .inline)
+                    .navigationBarItems(trailing: Button(action: {
+                        self.smartRecommendations.toggle()
+                    }) {
+                        Text(self.smartRecommendations ? "All" : "Best")
+                    })
                     .onAppear {
                         if self.selectedDay == -1 {
                             self.selectedDay = self.eventDays.first ?? -1
